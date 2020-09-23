9a2483bb59758949b7c887c75f3059b363b6014e
Yunus Gunes <gunesyu@Yunuss-MacBook-Pro.local>
2020-09-23 04:05:43 +0300

    Updates

---
+++ precache-manifest.05899822b9a8eff5776535fbac4a7e8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2afceea32396ead566cd50374ab3fb88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f69ce6e538b27ef6195",
+    "url": "/static/css/main.5d747303.chunk.css"
+  },
+  {
+    "revision": "1f219ad10f240919af55",
+    "url": "/static/js/2.1a9dd3cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a9dd3cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1ce7e992bc5ffb2c092",
+    "url": "/static/js/3.c3566920.chunk.js"
+  },
+  {
+    "revision": "1f69ce6e538b27ef6195",
+    "url": "/static/js/main.bd39e795.chunk.js"
+  },
+  {
+    "revision": "b48f30773f0871514670",
+    "url": "/static/js/runtime-main.9fe2a32d.js"
+  }
+]);
