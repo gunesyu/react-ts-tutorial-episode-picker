e964a4674f6e18045ee836ff39b6a854b93bcc77
Yunus Gunes <gunesyu@Yunuss-MacBook-Pro.local>
2020-09-23 04:08:26 +0300

    Updates

---
+++ precache-manifest.9247eabaa3707f59da9f6d4ffefeed7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9728dbe904930dd18023c73744274e",
+    "url": "/react-ts-tutorial-episode-picker/index.html"
+  },
+  {
+    "revision": "d302c5fb64203c4fc857",
+    "url": "/react-ts-tutorial-episode-picker/static/css/main.5d747303.chunk.css"
+  },
+  {
+    "revision": "6a5d4761bb9f9faac6fd",
+    "url": "/react-ts-tutorial-episode-picker/static/js/2.2d21c52a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ts-tutorial-episode-picker/static/js/2.2d21c52a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb261eb292dec502ac1",
+    "url": "/react-ts-tutorial-episode-picker/static/js/3.3a2f7caf.chunk.js"
+  },
+  {
+    "revision": "d302c5fb64203c4fc857",
+    "url": "/react-ts-tutorial-episode-picker/static/js/main.67da0e24.chunk.js"
+  },
+  {
+    "revision": "b5db3e965e866b9ae66a",
+    "url": "/react-ts-tutorial-episode-picker/static/js/runtime-main.7f9f8502.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05899822b9a8eff5776535fbac4a7e8f.js"
+  "/react-ts-tutorial-episode-picker/precache-manifest.9247eabaa3707f59da9f6d4ffefeed7a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ts-tutorial-episode-picker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
