ce1d6ea6f1b40eb2382fbd010abed6325c583bee
Yunus Emre Güneş <gunesyunusemre@gmail.com>
2021-08-17 12:26:27 +0300

    Updates

---
+++ precache-manifest.297ef55ca94b319eeb6f9036c4d25038.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a508d454f40c2589e329fcc9806bf68a",
+    "url": "/episode-picker/index.html"
+  },
+  {
+    "revision": "dd5abb02f432d66e249e",
+    "url": "/episode-picker/static/css/main.5d747303.chunk.css"
+  },
+  {
+    "revision": "470672da9d05741bb8a9",
+    "url": "/episode-picker/static/js/2.48657291.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/episode-picker/static/js/2.48657291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a83224aaffb9882916",
+    "url": "/episode-picker/static/js/3.22ce7039.chunk.js"
+  },
+  {
+    "revision": "dd5abb02f432d66e249e",
+    "url": "/episode-picker/static/js/main.05587138.chunk.js"
+  },
+  {
+    "revision": "59528c9a3ee92072aa30",
+    "url": "/episode-picker/static/js/runtime-main.b96feafd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-ts-tutorial-episode-picker/precache-manifest.7f226909ddd41b4772689b3fbcef4bac.js"
+  "/episode-picker/precache-manifest.297ef55ca94b319eeb6f9036c4d25038.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-ts-tutorial-episode-picker/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/episode-picker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
